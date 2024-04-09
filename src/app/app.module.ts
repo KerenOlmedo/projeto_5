d5699ed5c71aab92675356f972620a2ca403e4d4
Kéren Olmedo <keren.olmedo09@gmail.com>
2024-04-08 23:53:48 -0300

    feat: create components initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InitialScreenComponent } from './components/initial-screen/initial-screen.component';
+import { LoginComponent } from './components/login/login.component';
+import { CadastroComponent } from './components/cadastro/cadastro.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InitialScreenComponent,
+    LoginComponent,
+    CadastroComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/initial-screen/initial-screen.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-initial-screen',
+  templateUrl: './initial-screen.component.html',
+  styleUrls: ['./initial-screen.component.scss']
+})
+export class InitialScreenComponent {
+
+}
